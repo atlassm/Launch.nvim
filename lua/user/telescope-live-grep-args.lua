7370ef9d96cc3c0e2750166197aac7a1048e9ccc
Shlomi Meiri <shlomime@amazon.com>
2024-07-19 19:28:45 +0300

    indent some files and add C-c functionality in telescope.
    add untracked files

---
+++ lua/user/telescope-live-grep-args.lua
@@ -0,0 +1,19 @@
+local M = {
+	"nvim-telescope/telescope.nvim",
+}
+
+function M.config()
+
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>gdo"] = { "<cmd>DiffviewOpen<cr>", "Diff view Git Diff Open" },
+    ["<leader>gdc"] = { "<cmd>DiffviewClose<cr>", "Diff view Git Diff Close" },
+    ["<leader>gfh"] = { "<cmd>DiffviewFileHistory %<cr>", "diff view Git File History" },
+    ["<leader>grh"] = { "<cmd>DiffviewFileHistory<cr>", "diff view Git Repo History" },
+  }
+  local diffview = require "diffview"
+  diffview.setup {}
+
+end
+
+return M
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -2,15 +2,15 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 local M = {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
 		{ "nvim-telescope/telescope-live-grep-args.nvim" ,
-            -- This will not install any breaking changes.
-            -- For major updates, this must be adjusted manually.
-            version = "^1.0.0",
-        },
-  },
+			-- This will not install any breaking changes.
+			-- For major updates, this must be adjusted manually.
+			version = "^1.0.0",
+		},
+	},
 }
 
 function vim.getVisualSelection()
@@ -27,145 +27,149 @@ function vim.getVisualSelection()
 end
 
 function M.config()
-  local wk = require "which-key"
-  wk.register {
-    ["<leader><Space>"] = { "<cmd>Telescope buffers previewer=true<cr>", "Opened buffers" },
-    ["<leader>fb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    ["<leader>fc"] = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find files" },
-    ["<leader>fp"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
-    ["<leader>ft"] = { "<cmd>Telescope live_grep<cr>", "Find Text (live_grep)" },
-    ["<leader>fs"] = { "<cmd>Telescope grep_string<cr>", "Find String (grep_string)" },
-    ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help" },
-    ["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
-    ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
-    ["<leader>bn"] = { "<cmd>bn<cr>", "next buffer" },
-    ["<leader>bp"] = { "<cmd>bp<cr>", "prev buffer" },
---    ["<leader>*"] = { ":%s/\<C-r><C-w>\>/", "Substitute" },
-  }
+	local wk = require "which-key"
+	wk.register {
+		["<leader><Space>"] = { "<cmd>Telescope buffers previewer=true<cr>", "Opened buffers" },
+		["<leader>fb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+		["<leader>fc"] = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+		["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find files" },
+		["<leader>fp"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+		["<leader>ft"] = { "<cmd>Telescope live_grep<cr>", "Find Text (live_grep)" },
+		["<leader>fs"] = { "<cmd>Telescope grep_string<cr>", "Find String (grep_string)" },
+		["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help" },
+		["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
+		["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
+		["<leader>bn"] = { "<cmd>bn<cr>", "next buffer" },
+		["<leader>bp"] = { "<cmd>bp<cr>", "prev buffer" },
+		--    ["<leader>*"] = { ":%s/\<C-r><C-w>\>/", "Substitute" },
+	}
 
-  local icons = require "user.icons"
-  local actions = require "telescope.actions"
+	local icons = require "user.icons"
+	local actions = require "telescope.actions"
+	local function execute_norm(prompt_bufnr)
+	  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<ESC>', true, false, true), 'n', true)
+end
 
+	require("telescope").setup {
+		defaults = {
+			prompt_prefix = icons.ui.Telescope .. " ",
+			selection_caret = icons.ui.Forward .. " ",
+			entry_prefix = "   ",
+			initial_mode = "insert",
+			selection_strategy = "reset",
+			path_display = { "absolute" },
+			color_devicons = true,
+			vimgrep_arguments = {
+				"rg",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+				"--hidden",
+				"--glob=!.git/",
+				"--glob=!part-support/",
+			},
 
-  require("telescope").setup {
-    defaults = {
-      prompt_prefix = icons.ui.Telescope .. " ",
-      selection_caret = icons.ui.Forward .. " ",
-      entry_prefix = "   ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      path_display = { "absolute" },
-      color_devicons = true,
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--hidden",
-        "--glob=!.git/",
-      },
+			mappings = {
+				i = {
+					["<C-n>"] = actions.cycle_history_next,
+					["<C-p>"] = actions.cycle_history_prev,
 
-      mappings = {
-        i = {
-          ["<C-n>"] = actions.cycle_history_next,
-          ["<C-p>"] = actions.cycle_history_prev,
+					["<C-j>"] = actions.move_selection_next,
+					["<C-k>"] = actions.move_selection_previous,
+					["<C-c>"] = execute_norm,
+				},
+				n = {
+					["<esc>"] = actions.close,
+					["j"] = actions.move_selection_next,
+					["k"] = actions.move_selection_previous,
+					["q"] = actions.close,
+				},
+			},
+		},
+		pickers = {
+			initial_mode = "normal",
+			--      live_grep = {
+			--        theme = "dropdown",
+			--      },
 
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-        },
-        n = {
-          ["<esc>"] = actions.close,
-          ["j"] = actions.move_selection_next,
-          ["k"] = actions.move_selection_previous,
-          ["q"] = actions.close,
-        },
-      },
-    },
-    pickers = {
-        initial_mode = "normal",
---      live_grep = {
---        theme = "dropdown",
---      },
+			grep_string = {
+				--        theme = "dropdown",
+				initial_mode = "normal",
+			},
 
-      grep_string = {
---        theme = "dropdown",
-        initial_mode = "normal",
-      },
+			live_grep = {
+				--        theme = "dropdown",
+				-- initial_mode = "insert",
+			},
+			find_files = {
+				path_display = {"absolute"},
+				--        theme = "dropdown",
+				--        previewer = false,
+			},
+			oldfiles = {
+				initial_mode = "normal",
+			},
+			buffers = {
+				--        theme = "dropdown",
+				previewer = true,
+				initial_mode = "normal",
+				-- scroll_strategy = "cycle", -- not cycling through list --[[ limit ]]
+				scroll_strategy = "limit", -- not cycling through list --[[ limit ]]
+				sorting_strategy = "descending",
+				sort_lastused = true, -- more logical
+				mappings = {
+					i = {
+						["<C-d>"] = actions.delete_buffer,
+					},
+					n = {
+						["dd"] = actions.delete_buffer,
+					},
+				},
+			},
 
-      live_grep = {
---        theme = "dropdown",
-        -- initial_mode = "insert",
-      },
-      find_files = {
-		  path_display = {"absolute"},
---        theme = "dropdown",
---        previewer = false,
-      },
-      oldfiles = {
-        initial_mode = "normal",
-      },
-      buffers = {
---        theme = "dropdown",
-    	previewer = true,
-        initial_mode = "normal",
-		-- scroll_strategy = "cycle", -- not cycling through list --[[ limit ]]
-		scroll_strategy = "limit", -- not cycling through list --[[ limit ]]
-		sorting_strategy = "descending",
-		sort_lastused = true, -- more logical
-        mappings = {
-          i = {
-            ["<C-d>"] = actions.delete_buffer,
-          },
-          n = {
-            ["dd"] = actions.delete_buffer,
-          },
-        },
-      },
+			planets = {
+				show_pluto = true,
+				show_moon = true,
+			},
 
-      planets = {
-        show_pluto = true,
-        show_moon = true,
-      },
+			colorscheme = {
+				enable_preview = true,
+			},
 
-      colorscheme = {
-        enable_preview = true,
-      },
+			lsp_references = {
+				previewer = true,
 
-      lsp_references = {
-    	previewer = true,
+				-- theme = "dropdown",
+				initial_mode = "normal",
+			},
 
-        -- theme = "dropdown",
-        initial_mode = "normal",
-      },
+			lsp_definitions = {
+				-- theme = "dropdown",
+				initial_mode = "normal",
+			},
 
-      lsp_definitions = {
-        -- theme = "dropdown",
-        initial_mode = "normal",
-      },
+			lsp_declarations = {
+				-- theme = "dropdown",
+				initial_mode = "normal",
+			},
 
-      lsp_declarations = {
-        -- theme = "dropdown",
-        initial_mode = "normal",
-      },
-
-      lsp_implementations = {
-        -- theme = "dropdown",
-        initial_mode = "normal",
-      },
-    },
-    extensions = {
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-      },
-    },
-  }
+			lsp_implementations = {
+				-- theme = "dropdown",
+				initial_mode = "normal",
+			},
+		},
+		extensions = {
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			},
+		},
+	}
 
 	require("telescope").load_extension("live_grep_args")
 
@@ -173,14 +177,14 @@ function M.config()
 
 	-- keymap('n', '<space>g', ':Telescope current_buffer_fuzzy_find<cr>', opts)
 	-- keymap('v', '<space>g', function()
-		-- 	local text = vim.getVisualSelection()
-		-- 	tb.current_buffer_fuzzy_find({ default_text = text })
-		-- end, opts)
+	-- 	local text = vim.getVisualSelection()
+	-- 	tb.current_buffer_fuzzy_find({ default_text = text })
+	-- end, opts)
 
-		keymap('v', '<leader>ft', function()
-			local text = vim.getVisualSelection()
-			tb.live_grep({ default_text = text })
-		end, opts)
+	keymap('v', '<leader>ft', function()
+		local text = vim.getVisualSelection()
+		tb.live_grep({ default_text = text })
+	end, opts)
 
 end
 
