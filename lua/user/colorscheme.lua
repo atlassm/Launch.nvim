a0e19f8503557198e0da9f49f2fce1653015e143
Shlomi Meiri <shlomime@amazon.com>
2024-09-28 18:12:43 +0300

    change color scheme to catpuccin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,14 @@
 local M = {
-  "LunarVim/darkplus.nvim",
+  -- "LunarVim/darkplus.nvim",
+  "catppuccin/nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  -- vim.cmd.colorscheme "darkplus"
+  vim.cmd.colorscheme "catppuccin"
+  vim.g.catppuccin_flavor = "mocha" -- or "latte", "frappe", "macchiato"
 -- -- Set background colors for diff highlighting
 -- vim.cmd('highlight DiffAdd guibg=#00cc00')
 -- vim.cmd('highlight DiffChange guibg=#001C36')
